f51d641386ecd6fa2aab80089e9c06db6df68d76
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-12 15:05:32 +0800

    feat: finish tutorial for deployment

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm a Computer Science student and Web Developer based in Cebu,
+          Philippines. I design and develop modern web applications with a focus
+          on great UI/UX without compromising performance or accessibility.
+        </p>
+        <p>
+          You can find me organizing extracurricular activities in my
+          university's tech organizations. I am always looking for new
+          challenges and opportunities to learn and grow as a developer, one
+          line of code at a time.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
